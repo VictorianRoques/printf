d6b393aa6973cb5554eadedcb890f72c62cf4113
Victorian ROQUES <viroques@e1r11p15.42.fr>
2020-02-14 01:34:22 +0100

    need to handle minus precision padding and p flag

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 /*   By: viroques <viroques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/09 19:56:23 by viroques          #+#    #+#             */
-/*   Updated: 2019/12/20 18:30:58 by viroques         ###   ########.fr       */
+/*   Updated: 2020/02/14 01:31:48 by viroques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/ft_printf.h"
+#include "Includes/ft_printf.h"
 #include <stdio.h>
 
 int		main()
@@ -19,7 +19,12 @@ int		main()
 	int s[10];
 
 	*s = 10;
+	// ft_printf("%-*.*d\n",10, 20, 10);
+	// printf("%-*.*d\n",10, 20, 10);
 
-	printf("%.2d", 16789);
-	// ft_printf("%.2d", 177);
+	// ft_printf("%.5s\n", str);
+	// printf("%.5s\n", str);
+
+	// ft_printf("%-10.2i\n", 18745);
+	printf("%-10.2i\n", 18745);
 }
--- srcs/ft_printf_conv1.c
+++ srcs/ft_printf_conv1.c
@@ -6,28 +6,46 @@
 /*   By: viroques <viroques@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 12:33:58 by viroques          #+#    #+#             */
-/*   Updated: 2019/12/20 18:27:01 by viroques         ###   ########.fr       */
+/*   Updated: 2020/02/14 01:33:16 by viroques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void		ft_handle_buff_padding(t_env *env, int len)
+{
+	if (env->attributs.zero == true)
+		ft_fill_padding(env, env->width.padding - len, '0');
+	else
+		ft_fill_padding(env, env->width.padding - len, ' ');
+}
+
 void		ft_handle_buff(t_env *env, char *str, int len)
 {
 	if (env->attributs.minus == true)
 	{
+		if (env->width.precision > len)
+		{
+			ft_fill_padding(env, env->width.precision - len, '0');
+		}
 		ft_fill_buff_s(env, str, len);
-		if (env->width.padding > len)
-			ft_fill_padding(env, env->width.padding - len, ' ');
+		if (env->width.padding > len && env->width.precision > len)
+			ft_handle_buff_padding(env, env->width.precision);
+		else if (env->width.padding > len)
+			ft_handle_buff_padding(env, env->width.precision - len);
+		// gerer mieux le minus le pading pue la merde
 	}
 	else
 	{
 		if (env->width.padding > len)
 		{
-			if (env->attributs.zero == true)
-				ft_fill_padding(env, env->width.padding - len, '0');
-			else	
-				ft_fill_padding(env, env->width.padding - len, ' ');
+			if (env->width.precision > len)
+			{
+				ft_handle_buff_padding(env, env->width.precision);
+				ft_fill_padding(env, env->width.precision - len, '0');
+			}
+			else
+				ft_handle_buff_padding(env, len);
 		}
 		ft_fill_buff_s(env, str, len);
 	}
@@ -69,9 +87,9 @@ int		ft_x_conv(t_env *env, unsigned int nb, int x)
 	int		len;
 
 	if (x == 0)
-		str = ft_itoa_base_unsigned(nb, "0123456789abcdef");
+		str = ft_itoa_base(nb, "0123456789abcdef");
 	else
-		str = ft_itoa_base_unsigned(nb, "0123456789ABCDEF");
+		str = ft_itoa_base(nb, "0123456789ABCDEF");
 	len = ft_strlen(str);
 	ft_handle_buff(env, str, len);
 	return (0);
@@ -82,8 +100,8 @@ int		ft_p_conv(t_env *env, void *p)
 	char	*str;
 	int		len;
 	static char *hexa = "0x";
-	
-	str = ft_itoa_base_unsigned((unsigned int)p, "0123456789abcdef");
+
+	str = ft_itoa_base((unsigned int)p, "0123456789abcdef");
 	len = ft_strlen(str);
 	if (env->attributs.minus == true)
 	{
